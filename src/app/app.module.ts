744f187e812eeeb58236832f423fbba45861192c
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-09 00:38:50 +0530

    error edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
